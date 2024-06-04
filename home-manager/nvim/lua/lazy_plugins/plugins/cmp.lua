7bd64f24d881324997ac1170d2f7a9d7915ddf99
Marius Niemenmaa <marius.niemenmaa@outlook.com>
2024-06-04 19:43:28 +0300

    Split lsp to cmp and mason

--- home-manager/nvim/lua/lazy_plugins/plugins/cmp.lua
+++ home-manager/nvim/lua/lazy_plugins/plugins/cmp.lua
@@ -1,41 +1,45 @@
 return {
-  "williamboman/mason.nvim",
+  'hrsh7th/nvim-cmp',
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "hrsh7th/cmp-nvim-lsp",
+    'L3MON4D3/LuaSnip',
+    'hrsh7th/cmp-nvim-lsp',
+    'saadparwaiz1/cmp_luasnip',
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
     "rafamadriz/friendly-snippets"
   },
+
   config = function()
+    require("luasnip.loaders.from_vscode").lazy_load()
+
     local cmp = require("cmp")
     cmp.setup({
       snippet = {
         expand = function(args)
-          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          require('luasnip').lsp_expand(args.body)
         end,
       },
-      mapping = cmp.mapping.preset.insert({
+      mapping = {
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      }),
+        ['<CR>'] = cmp.mapping.confirm({
+          select = true,
+        }),
+      },
+
+
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' }, -- For luasnip users.
+
       }, {
         { name = 'buffer' },
         { name = 'path' },
       })
     })
-
     -- Set configuration for specific filetype.
     cmp.setup.filetype('gitcommit', {
       sources = cmp.config.sources({
@@ -63,29 +67,5 @@ return {
       }),
       matching = { disallow_symbol_nonprefix_matching = false }
     })
-
-    -- Set up lspconfig.
-    local capabilities = vim.tbl_deep_extend("force", vim.lsp.protocol.make_client_capabilities(),
-      require('cmp_nvim_lsp').default_capabilities())
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls"
-      },
-      handlers = {
-        function(server_name)
-          require("lspconfig")[server_name].setup {
-            capabilities = capabilities,
-          }
-        end,
-      }
-    })
-    require("luasnip.loaders.from_vscode").lazy_load()
-
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-        update_in_insert = true,
-      }
-    )
   end
 }
