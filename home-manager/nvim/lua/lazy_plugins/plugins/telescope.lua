d815aca811ac0faecd8f8dca4e534f66ef4af84e
Marius Niemenmaa <marius.niemenmaa@outlook.com>
2024-06-04 19:43:46 +0300

    Common search binding for telsecope

--- home-manager/nvim/lua/lazy_plugins/plugins/telescope.lua
+++ home-manager/nvim/lua/lazy_plugins/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
     local builtin = require('telescope.builtin')
 
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find in Files' })
+    vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Find in Files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find in Buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
