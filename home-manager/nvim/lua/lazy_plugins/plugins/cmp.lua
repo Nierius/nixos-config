7524280299393be0551aa4767ade8a3b0d73c797
Marius Niemenmaa <marius.niemenmaa@outlook.com>
2024-06-25 19:00:16 +0300

    Completion styling

--- home-manager/nvim/lua/lazy_plugins/plugins/cmp.lua
+++ home-manager/nvim/lua/lazy_plugins/plugins/cmp.lua
@@ -4,16 +4,20 @@ return {
     'L3MON4D3/LuaSnip',
     'hrsh7th/cmp-nvim-lsp',
     'saadparwaiz1/cmp_luasnip',
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "rafamadriz/friendly-snippets"
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
+    'rafamadriz/friendly-snippets',
+    'onsails/lspkind-nvim'
   },
 
-  config = function()
-    require("luasnip.loaders.from_vscode").lazy_load()
 
-    local cmp = require("cmp")
+  config = function()
+    require('luasnip.loaders.from_vscode').lazy_load()
+
+    local lspkind = require('lspkind')
+    local cmp = require('cmp')
     cmp.setup({
       snippet = {
         expand = function(args)
@@ -21,6 +25,8 @@ return {
         end,
       },
       mapping = {
+        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
@@ -34,11 +40,26 @@ return {
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' }, -- For luasnip users.
-
+        { name = 'nvim_lsp_signature_help' }
       }, {
         { name = 'buffer' },
         { name = 'path' },
-      })
+      }),
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol_text', -- show only symbol annotations
+          maxwidth = 60,        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          -- can also be a function to dynamically calculate max width such as
+          -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+          ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          before = function(entry, vim_item)
+            return vim_item
+          end
+        })
+      }
     })
     -- Set configuration for specific filetype.
     cmp.setup.filetype('gitcommit', {
