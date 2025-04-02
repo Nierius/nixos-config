4d99604886353d49cac0208caf78c0630967d905
Marius Niemenmaa <marius.niemenmaa@outlook.com>
2025-04-02 21:41:48 +0300

    Docker compose as part of docker conf

--- nixos/core/docker.nix
+++ nixos/core/docker.nix
@@ -1,8 +1,11 @@
-{ config, lib, ... }:
+{ pkgs, config, lib, ... }:
 
 with lib;
 
 mkIf config.requireDocker {
   # Enable docker
   virtualisation.docker.enable = true;
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
 }
